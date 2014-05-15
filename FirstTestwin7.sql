c1e3bf59c4ca8505b8da2824877fb64c60915329
Jada Potata <williamsj26@udayton.edu>
2014-05-15 14:36:01 -0400

    Well this is the first commit and push to heroku

---
+++ FirstTestwin7.sql
@@ -0,0 +1,14 @@
+﻿-- Database: dakd2njmgah4l1
+
+-- DROP DATABASE dakd2njmgah4l1;
+
+CREATE DATABASE dakd2njmgah4l1
+  WITH OWNER = shxvinzncwhrkv
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
+GRANT ALL ON DATABASE dakd2njmgah4l1 TO shxvinzncwhrkv;
+REVOKE ALL ON DATABASE dakd2njmgah4l1 FROM public;
+
